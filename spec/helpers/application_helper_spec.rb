415f32250c07e5dae559f84df47e89b301573850
TomKo1 <kottomasz98@gmail.com>
2018-08-23 15:01:45 +0200

    Added travis config

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe ApplicationHelper, type: :helper do
+    # just to test travis
+  describe '#test the truth' do
+    it 'returns true' do
+    end
+  end
+end
