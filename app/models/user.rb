7cb14769332d391d1d6da7e175030f8f60ffbeb1
Superek1993 <dzikimiki93@gmail.com>
2018-08-24 11:54:57 +0200

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, length: { in: 2..20 }
 end
