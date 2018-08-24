24c2cc7c1185cd942df7ec0c4e6369b16fe84f04
TomKo1 <kottomasz98@gmail.com>
2018-08-24 11:39:01 +0200

    created-many-to-many-group-user

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
     validates_presence_of :name
     validates :name, length: { in: 5..255 }
+    has_many :group_users
+    has_many :users, through: :group_users
 end
