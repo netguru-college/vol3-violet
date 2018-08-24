6dc963083a1451564e17de9ae5fc88401b007875
Superek1993 <dzikimiki93@gmail.com>
2018-08-24 12:47:01 +0200

    add username field to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permited_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permited_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
 end
