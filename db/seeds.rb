38c137496948aad6a06af326403940d707aa7bf9
TomKo1 <kottomasz98@gmail.com>
2018-08-26 21:06:25 +0200

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
 require 'faker'
 
-User.find_or_create_by(email: 'user2@bollywood.com') do |user|
+User.find_or_create_by(email: 'user@aa.pl') do |user|
+    user.username = 'user'
     user.password = "123456"
+end
+
+User.find_or_create_by(email: 'admin@aa.pl') do |user|
+    user.username = 'admin'
+    user.admin = true
+    user.password = 'admin123'
+end
+
+5.times do
+    User.find_or_create_by(email: "#{Faker::Hacker.adjective}@aa.pl") do |user|
+        user.username = Faker::Hacker.adjective
+        user.password = '123456'
+    end
 end
