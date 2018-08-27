2009e95a6fbf7b3d633b23b2ca7a988bb59f38c8
TomKo1 <kottomasz98@gmail.com>
2018-08-27 10:32:15 +0200

    Fixed inverse of

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,5 +1,5 @@
 class Debt < ApplicationRecord
   belongs_to :bill
   has_one :payer, through: :bill
-  belongs_to :borrower, foreign_key: 'user_id', class_name: 'User', inverse_of: :user
+  belongs_to :borrower, foreign_key: 'user_id', class_name: 'User', inverse_of: :debt
 end
