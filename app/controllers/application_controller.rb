d8b9a9c0e41963492bfa21d14db9495c494adcd8
TomKo1 <kottomasz98@gmail.com>
2018-08-27 14:01:55 +0200

    User not able to log when blocked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permited_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :blocked?
 
   protected
 
@@ -8,5 +9,13 @@ class ApplicationController < ActionController::Base
     added_attrs = %i[username email password password_confirmation avatar avatar_cache remove_avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def blocked?
+    return if current_user.blank? || !current_user.blocked?
+    sign_out current_user
+    redirect_to new_user_session_path, alert: 'Account suspended'
   end
 end
