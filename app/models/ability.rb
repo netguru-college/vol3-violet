4fbc8c9f292f56af92bdae740476436274cc2af4
TomKo1 <kottomasz98@gmail.com>
2018-08-27 15:46:03 +0200

    Fixed rubocop issues

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,8 +1,17 @@
 class Ability
   include CanCan::Ability
 
+  attr_reader :user
+
   def initialize(user)
-    can :ban, User if current_user.admin? && current_user.present? && !user.admin?
-    can :destroy, User if current_user.admin? && current_user.present? && !user.admin?
+    @user = user
+    can :ban, User if admin?
+    can :destroy, User if admin?
+  end
+
+  private
+
+  def admin?
+    current_user.admin? && current_user.present? && !user.admin?
   end
 end
