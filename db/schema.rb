06e6e19129e45a6582ba90a056647f9de93c36e3
Superek1993 <dzikimiki93@gmail.com>
2018-08-27 11:12:02 +0200

    schema changed due to migration

--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,6 @@ ActiveRecord::Schema.define(version: 2018_08_26_161517) do
     t.index ["user_id"], name: "index_debts_on_user_id"
   end
 
-  create_table "group_models", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "group_users", force: :cascade do |t|
     t.integer "user_id"
     t.integer "group_id"
