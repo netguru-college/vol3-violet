884ed5c16a5daf5ab3cf495b990b8e3616db75b0
TomKo1 <kottomasz98@gmail.com>
2018-08-27 15:30:01 +0200

    Fixed bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permited_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :blocked?
+  before_action :check_blocked
 
   protected
 
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def blocked?
+  def check_blocked
     return if current_user.blank? || !current_user.blocked?
     sign_out current_user
     redirect_to new_user_session_path, alert: 'Account suspended'
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :ban, User if user.admin? && user.present?
-    can :destroy, User if user.admin? && user.present?
+    can :ban, User if current_user.admin? && current_user.present? && !user.admin?
+    can :destroy, User if current_user.admin? && current_user.present? && !user.admin?
   end
 end
