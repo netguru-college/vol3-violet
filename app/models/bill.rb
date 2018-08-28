ac69e1badee520d85e63089fb72b4cdd6a76123c
Kuba Semczuk <JakubSemczuk@gmail.com>
2018-08-28 10:43:27 +0200

    Fix relations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,5 +1,11 @@
 class Bill < ApplicationRecord
-  belongs_to :payer, foreign_key: 'user_id', class_name: 'User', inverse_of: :bill
-  has_many   :debts
-  has_many   :borrowers, through: :debts
+  belongs_to :payer,      class_name:  'User',
+                         # source:      :user,
+                          inverse_of:  :bills_as_payer
+  has_many   :debts,      class_name:  'Debt',
+                          foreign_key: 'bill_id',
+                          inverse_of:  :bill
+  has_many   :borrowers,  through:     :debts,
+                          source:      :borrower,                   # or class_name: 'User'
+                          inverse_of:  :bills_as_borrower           # or through: :debts_as_borrower
 end
