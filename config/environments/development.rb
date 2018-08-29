01d85fd5f884be5d06e288a363e224fb8549df7d
Superek1993 <dzikimiki93@gmail.com>
2018-08-29 23:21:18 +0200

    setup development host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.carrierwave_storage = :file
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
