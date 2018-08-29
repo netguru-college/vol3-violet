ecbdd7d6d9bfa3d50c91df5b57bc6b1a56d64451
TomKo1 <kottomasz98@gmail.com>
2018-08-29 12:18:26 +0200

    Travis_v1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.carrewave_storage = :dropbox
+  config.carrierwave_storage = :dropbox
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
 CarrierWave.configure do |config|
-    config.storage = Rails.configuration.carrewave_storage
-    config.dropbox_access_token = Rails.application.credentials[:dropbox][:access_key]
+  config.storage = Rails.configuration.carrierwave_storage
+  config.dropbox_access_token = Rails.application.credentials[:dropbox][:access_key]
 end
