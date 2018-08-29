ec6b98186541073d373c406b7eb43cf2d7c01fd8
TomKo1 <kottomasz98@gmail.com>
2018-08-29 11:37:48 +0200

    Modified config

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :dropbox
+  # storage :dropbox
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,4 @@
 CarrierWave.configure do |config|
+    config.storage = Rails.configuration.carrewave_storage
     config.dropbox_access_token = Rails.application.credentials[:dropbox][:access_key]
 end
