1efd3ebd9255ab1cd87624f739fd4131657f2966
TomKo1 <kottomasz98@gmail.com>
2018-08-29 15:42:07 +0200

    Added host to link to

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,10 +2,9 @@ Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'nccappviolet.herokuapp.com' }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
